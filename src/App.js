f2e1dc1a88990c7fa47f39cfd7f241e40544db4c
ricardo <ricardo-svc@live.com>
2020-04-10 22:35:27 -0300

    criando o hooks da maquina agricola JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import useKonamiCode from './useKonamiCode';
 
 function App() {
-  return <div></div>;
+  const { sequence, rightSequence } = useKonamiCode();
+
+  return (
+    <div>
+      <h1>Maquina Agricola JS</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Konami_Code.svg"
+        alt="Konami Code!"
+      />
+
+      <p>
+        {sequence.map((key, i) => (
+          <b key={i}>{key}</b>
+        ))}
+      </p>
+      {rightSequence && (
+        <img
+          src="https://www.rescuecursos.com/wp-content/uploads/elementor/thumbs/MAQUINA-AGROCOLA-nzdl6g8uqamfdtjqdvex1vz0hinsm6kvjoxrlryor4.jpg"
+          alt="MAQUÍNA AGRÍCOLA"
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/useKonamiCode.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+export const konamiSequence = [
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'b',
+  'a'
+];
+
+export default function useKonamiCode(
+  codeSequence = konamiSequence,
+  callback = () => {}
+) {
+  const [rightSequence, setRightSequence] = useState(false);
+  const [sequence, setSequence] = useState([]);
+
+  const onKeyDown = (event) => setSequence((prev) => [...prev, event.key]);
+
+  useEffect(() => {
+    sequence.forEach((key, i) => {
+      if (key !== codeSequence[i]) {
+        setSequence([]);
+      }
+    });
+
+    if (sequence.toString() === codeSequence.toString()) {
+      setRightSequence(true);
+      callback();
+    }
+  }, [sequence]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+    return () => window.removeEventListener('keydown', onKeyDown);
+  });
+
+  return { sequence, rightSequence };
+}
